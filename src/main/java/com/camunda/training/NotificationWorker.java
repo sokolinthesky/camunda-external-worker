41f58176397fcead15b0fc5d54cef9754ee557bf
Oleh_Soklakov <oleh_soklakov@epam.com>
2023-12-25 14:20:11 +0100

    added worker

---
+++ src/main/java/com/camunda/training/NotificationWorker.java
@@ -0,0 +1,34 @@
+package com.camunda.training;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import org.camunda.bpm.client.ExternalTaskClient;
+import org.camunda.bpm.client.topic.TopicSubscriptionBuilder;
+
+public class NotificationWorker {
+
+  public static void main(String[] args) {
+    // bootstrap the client
+    ExternalTaskClient client = ExternalTaskClient.create()
+        .baseUrl("http://localhost:8080/engine-rest")
+        .asyncResponseTimeout(20000)
+        .lockDuration(10000)
+        .maxTasks(1)
+        .build();
+
+    // subscribe to the topic
+    TopicSubscriptionBuilder subscriptionBuilder = client.subscribe("notification");
+
+    // handle job
+    subscriptionBuilder.handler((externalTask, externalTaskService) -> {
+      String content = externalTask.getVariable("content");
+      System.out.println("Sorry, your tweet has been rejected: " + content);
+      Map<String, Object> variables = new HashMap<>();
+      variables.put("notficationTimestamp", new Date());
+      externalTaskService.complete(externalTask, variables);
+    });
+
+    subscriptionBuilder.open();
+  }
+}
